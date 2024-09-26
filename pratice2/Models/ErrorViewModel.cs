9116bee61c3f7ce53fd3a43617035d892b98d995
Mehedi15030 <mehedicse15030@gmail.com>
2024-09-26 10:34:31 +0600

    Add project files.

---
+++ pratice2/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace pratice2.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
